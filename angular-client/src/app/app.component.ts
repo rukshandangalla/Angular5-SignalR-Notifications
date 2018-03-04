407ddc6a1c30ad9b5633dc66f58e663c29c62562
Rukshan Dangalle <rukshand@lbfinance.local>
2018-03-04 21:47:14 +0530

    All completed

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
 
 import { Message } from 'primeng/api';
 
@@ -9,15 +10,20 @@ import { Message } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
+  private _hubConnection: HubConnection;
   msgs: Message[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this._hubConnection = new HubConnection('http://localhost:1874/notify');
+    this._hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
 
-  }
-
-  showMessage() {
-    this.msgs.push({ severity: 'success', summary: 'Success Message', detail: 'Order submitted' });
+    this._hubConnection.on('BroadcastMessage', (type: string, payload: string) => {
+      this.msgs.push({ severity: type, summary: payload });
+    });
   }
 }
